cfe5bfccb3654f7d842400253d48bf7ba8f196b8
dan28000 <84990628+dan28000@users.noreply.github.com>
2024-09-26 14:55:19 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,109 @@
+import io.papermc.paperweight.util.constants.PAPERCLIP_CONFIG
+
+plugins {
+    java
+    `maven-publish`
+    id("io.papermc.paperweight.patcher") version "1.7.2-SNAPSHOT"
+}
+
+val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/") {
+        content {
+            onlyForConfigurations(PAPERCLIP_CONFIG)
+        }
+    }
+}
+
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.10.3:fat")
+    decompiler("org.vineflower:vineflower:1.10.1")
+    paperclip("io.papermc:paperclip:3.0.3")
+}
+
+allprojects  {
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+}
+
+subprojects {
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(21)
+    }
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+
+    repositories {
+        mavenCentral()
+        maven(paperMavenPublicUrl)
+        maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
+        maven("https://repo.aikar.co/content/groups/aikar")
+        maven("https://repo.md-5.net/content/repositories/releases/")
+        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven("https://jitpack.io")
+    }
+}
+
+paperweight {
+    serverProject.set(project(":basiclandmc-server"))
+
+    remapRepo.set("https://maven.fabricmc.net/")
+    decompileRepo.set(paperMavenPublicUrl)
+
+    useStandardUpstream("divinemc") {
+        url.set(github("DivineMC", "DivineMC"))
+        ref.set(providers.gradleProperty("divineRef"))
+
+        withStandardPatcher {
+            apiSourceDirPath.set("DivineMC-API")
+            serverSourceDirPath.set("DivineMC-Server")
+
+            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
+            apiOutputDir.set(layout.projectDirectory.dir("BasicLandMC-API"))
+
+            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
+            serverOutputDir.set(layout.projectDirectory.dir("BasicLandMC-Server"))
+        }
+
+        patchTasks.register("generatedApi") {
+            isBareDirectory = true
+            upstreamDirPath = "paper-api-generator/generated"
+            patchDir = layout.projectDirectory.dir("patches/generated-api")
+            outputDir = layout.projectDirectory.dir("paper-api-generator/generated")
+        }
+    }
+}
+
+tasks.generateDevelopmentBundle {
+    apiCoordinates = "cz.basicland.mc:basiclandmc-api"
+    libraryRepositories.set(
+        listOf(
+            "https://repo.maven.apache.org/maven2/",
+            paperMavenPublicUrl
+        )
+    )
+}
+
+publishing {
+    if (project.providers.gradleProperty("publishDevBundle").isPresent) {
+        publications.create<MavenPublication>("devBundle") {
+            artifact(tasks.generateDevelopmentBundle) {
+                artifactId = "dev-bundle"
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+import java.util.Locale
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+}
+
+rootProject.name = "BasicLandMC"
+
+for (name in listOf("BasicLandMC-API", "BasicLandMC-Server", "paper-api-generator")) {
+    val projName = name.lowercase(Locale.ENGLISH)
+    include(projName)
+    findProject(":$projName")!!.projectDir = file(name)
+}
