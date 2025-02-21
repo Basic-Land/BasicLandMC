9a322bb554da50c9f70b664f1e5de89f77f3a61f
dan28000 <84990628+dan28000@users.noreply.github.com>
2025-02-21 20:49:39 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,12 +34,6 @@ paperweight {
             patchesDir = file("basiclandmc-api/divinemc-patches")
             outputDir = file("divinemc-api")
         }
-        patchDir("purpurApi") {
-            upstreamPath = "purpur-api"
-            excludes = listOf("build.gradle.kts", "build.gradle.kts.patch", "paper-patches")
-            patchesDir = file("basiclandmc-api/purpur-patches")
-            outputDir = file("purpur-api")
-        }
     }
 }
 
