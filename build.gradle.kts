a41751fdfeaae27aea1a96e919c5f188a02bbb1c
dan28000 <84990628+dan28000@users.noreply.github.com>
2025-02-21 18:28:17 +0100

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
 
 paperweight {
     upstreams.register("divinemc") {
-        repo = github("DivineMC", "DivineMC")
+        repo = github("BX-Team", "DivineMC")
         ref = providers.gradleProperty("divineRef")
 
         patchFile {
