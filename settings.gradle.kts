727224fc9b50c5f85eb0e3544aca5d6b1fdf025c
dan28000 <84990628+dan28000@users.noreply.github.com>
2025-08-09 13:11:28 +0200

    update upstream (DivineMC)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,18 +3,17 @@ import java.util.Locale
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        mavenLocal()
         maven("https://repo.papermc.io/repository/maven-public/")
     }
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "BasicLandMC"
 
-for (name in listOf("basiclandmc-api", "basiclandmc-server", "shuttle")) {
+for (name in listOf("basiclandmc-api", "basiclandmc-server")) {
     val projName = name.lowercase(Locale.ENGLISH)
     include(projName)
     findProject(":$projName")!!.projectDir = file(name)
