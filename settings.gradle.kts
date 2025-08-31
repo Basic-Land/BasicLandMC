f3a1a5622a77a58004a33d485bb78eb19c0f72c1
dan28000 <84990628+dan28000@users.noreply.github.com>
2025-08-31 14:50:46 +0200

    ASWM

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,3 +18,20 @@ for (name in listOf("basiclandmc-api", "basiclandmc-server")) {
     include(projName)
     findProject(":$projName")!!.projectDir = file(name)
 }
+
+include(":gradle:platform")
+include(":api")
+include(":core")
+include(":importer")
+include(":loaders")
+include(":plugin")
+include("loaders:mongo-loader")
+findProject(":loaders:mongo-loader")?.name = "mongo-loader"
+include("loaders:api-loader")
+findProject(":loaders:api-loader")?.name = "api-loader"
+include("loaders:file-loader")
+findProject(":loaders:file-loader")?.name = "file-loader"
+include("loaders:mysql-loader")
+findProject(":loaders:mysql-loader")?.name = "mysql-loader"
+include("loaders:redis-loader")
+findProject(":loaders:redis-loader")?.name = "redis-loader"
