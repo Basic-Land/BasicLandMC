ce6ddfa8794ccb9ecc398973f9151fe7e5a444e8
dan28000 <84990628+dan28000@users.noreply.github.com>
2026-07-28 16:03:35 +0200

    update impl for metrics

--- basiclandmc-server/src/main/java/cz/basicland/metrics/Metrics.java
+++ basiclandmc-server/src/main/java/cz/basicland/metrics/Metrics.java
@@ -1,6 +1,5 @@
 package cz.basicland.metrics;
 
-import com.mojang.logging.LogUtils;
 import cz.basicland.metrics.impl.Chunks;
 import cz.basicland.metrics.impl.MobCaps;
 import dev.cubxity.plugins.metrics.api.UnifiedMetrics;
@@ -10,17 +9,16 @@ import org.bukkit.plugin.RegisteredServiceProvider;
 import org.slf4j.Logger;
 
 public class Metrics {
-    private static final Logger LOGGER = LogUtils.getLogger();
-    public static void init() {
+    public static void init(Logger logger) {
         RegisteredServiceProvider<UnifiedMetrics> registration = Bukkit.getServer().getServicesManager().getRegistration(UnifiedMetrics.class);
 
         if (registration != null) {
             MetricsManager metricsManager = registration.getProvider().getMetricsManager();
             metricsManager.registerCollection(new MobCaps());
             metricsManager.registerCollection(new Chunks());
-            LOGGER.info("UnifiedMetrics hook has been initialized");
+            logger.info("UnifiedMetrics hook has been initialized");
             return;
         }
-        LOGGER.warn("UnifiedMetrics plugin not found, metrics will be disabled");
+        logger.warn("UnifiedMetrics plugin not found, metrics will be disabled");
     }
 }
